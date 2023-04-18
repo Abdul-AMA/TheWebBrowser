91d4c0b34a25c826f55de6f40570d97cfa4fc134
Abood K. AMA <abdamapro22@gmail.com>
2023-04-18 22:31:51 +0300

    working

--- src/main/java/com/example/thewebbrowser/MyWebApplication.java
+++ src/main/java/com/example/thewebbrowser/MyWebApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MyWebApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(MyWebApplication.class.getResource("myWeb-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(),1280,800);
+        stage.setTitle("The Halal Net");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/thewebbrowser/MyWebController.java
+++ src/main/java/com/example/thewebbrowser/MyWebController.java
@@ -1,14 +1,63 @@
 package com.example.thewebbrowser;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 
-public class HelloController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MyWebController implements Initializable {
     @FXML
-    private Label welcomeText;
+    private WebView webView;
+    private WebEngine webEngine;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    public AnchorPane base;
+    @FXML
+    public Button buttonBack;
+    @FXML
+    public Button buttonHome;
+    public Button buttonLoad;
+    public Button buttonReload;
+    public TextField textUrl;
+    public Label labelSearch;
+    public TextField textSearch;
+    public Label labelURL;
+    @FXML
+    private Label label1;
+
+    @FXML
+    protected void onReload(ActionEvent event) {
+
+        System.out.println("hello");
+    }
+    @FXML
+    protected void onLoad() {
+
+        webEngine.load("https://www.youtube.com/");
+    }
+    @FXML
+    protected void onHome(ActionEvent event) {
+
+        System.out.println("hello");
+    }
+    @FXML
+    protected void onBack(ActionEvent event) {
+
+        System.out.println("hello");
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        webEngine = webView.getEngine();
+        onLoad();
     }
 }
